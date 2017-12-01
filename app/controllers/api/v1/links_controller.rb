0a201d7f42571ac66ba0f30ab0e5bb569b3ae75e
Geyson <geysondaniel@gmail.com>
2017-11-30 21:32:28 -0300

    Buscando user por wallet_hash

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::LinksController < Api::V1::ApiController
   def start
     hash = params[:wallet_hash]
     user = User.by_whallet_hash(hash)
-    cone = Cone.random_avaible
+    cone = Cone.random_available
     respond_to do |format|
       format.html { render json: user }
     end
--- app/models/cone.rb
+++ app/models/cone.rb
@@ -2,5 +2,5 @@ class Cone < ApplicationRecord
   has_many :links
   has_many :users, through: :links
 
-  scope :random_avaible, -> { where() }
+  scope :random_available, -> { where() }
 end
